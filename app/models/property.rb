18f2c8035015b3d40a72d042eafd983d3177f053
Jeff Rohalla <jrohalla@uchicago.edu>
2014-02-18 17:12:59 -0600

    Adds validation to property model

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,7 @@
 class Property < ActiveRecord::Base
+	validates :name, :description, :beds, :baths, :square_feet, :price, :address, :city, :state, :zip_code, :image_url, presence: true
+	validates :image_url, allow_blank: true, format: {
+		with: %r{\.(gif|jpg|png)\Z}i, 
+		message: 'must be a URL for GIF, JPG or PNG image'
+	}
 end
