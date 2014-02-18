ae392a7c10209255ffae70343afffae77c8b9bb6
Jeff Rohalla <JeffRohalla@gmail.com>
2014-02-18 13:53:18 -0600

    Adds static file controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 RealEstateApp::Application.routes.draw do
+  root 'static_pages#home'
+  get "static_pages/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
