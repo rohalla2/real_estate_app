a49ee95dc37e2dce290631e0c15c874c280231ea
Jeff Rohalla <jrohalla@uchicago.edu>
2014-02-26 21:02:16 -0600

    Makes misc view changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,10 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @user = User.find(params[:id])
+    if session[:user_id] != @user.id
+      redirect_to properties_path, notice: "Not authorized"
+    end
   end
 
   # GET /users/new
@@ -28,6 +32,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        session[:user_id] = @user.id
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render action: 'show', status: :created, location: @user }
       else
