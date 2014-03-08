721065b47507762e71ea61fe1441c575ca8fb36e
Steve Bartfield <Stevo393@gmail.com>
2014-03-07 19:02:49 -0600

    Creates information pages, Updates front page links

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ RealEstateApp::Application.routes.draw do
 
   root 'static_pages#home'
   get "static_pages/home"
+  get "information" => 'static_pages#information'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
