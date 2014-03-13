85e654e771af9d5a0deab8ab99363aca4b3bc79a
Jeff Rohalla <jrohalla@uchicago.edu>
2014-03-12 19:51:04 -0500

    Updates and comments tenant_ofs

--- app/controllers/tenant_ofs_controller.rb
+++ app/controllers/tenant_ofs_controller.rb
@@ -1,48 +1,68 @@
 class TenantOfsController < ApplicationController
+  before_action :authorize
+  before_action :check_if_manager
   before_action :set_tenant_of, only: [:show, :edit, :update, :destroy]
   before_action :set_property
-  before_action :check_if_manager
+  
 
-  # GET /tenant_ofs
+  # Only allows access to /tenant_ofs if there is a QSP of ?propertyID=# attached
+  # and if the current user is authorized to see the property's info
   def index
-  end
-
-
-   # GET /tenant_ofs/1
-  def show
-  end
-
-
-  # GET /tenant_ofs/new
-  def new
-    @tenant_of = TenantOf.new
-    @property_id = params["propertyID"]
-  end
-
-  # POST /tenant_ofs
-  def create
-    @tenant_of = TenantOf.new
-    email = params["email"].downcase
-    user = User.find_by(email: email)
-    if user.nil?
-      @tenant_of.errors.add(:email, " value #{email} does not correspond to any user")
-    else
-      @tenant_of.user_id = user.id
-      @tenant_of.property_id = @property.id
+    if @property.nil? || @property.user.id != @user.id
+      redirect_to properties_path, notice: "Not Authorized"      
     end
-    
-    
-    if @tenant_of.errors.count == 0 && @tenant_of.save
-      redirect_to property_path(@property), notice: 'Tenant of was successfully created.'
-    else
-      render action: 'new'
-    end 
   end
 
-  # DELETE /tenant_ofs/1
+   # Only show if @user is the manager of the property for /tenant_ofs/#
+  def show
+    if @user.id != @tenant_of.user.id && @user.id != @tenant_of.property.user.id
+      redirect_to properties_path, notice: "Not Authorized"
+    end
+  end
+
+  # only allows managers to attempt to add tenants to their
+  # own properties
+  def new
+    if @property.user.id != @user.id
+      redirect_to properties_path, notice: "Not Authorized"
+    end
+
+    @tenant_of = TenantOf.new
+    
+  end
+
+  # Only allows managers to create tenants for their own properties
+  def create
+    if @property.user.id != @user.id
+      redirect_to properties_path, notice: "Not Authorized"
+    else
+      @tenant_of = TenantOf.new
+      email = params["email"].downcase
+      user = User.find_by(email: email)
+      
+      if user.nil?
+        @tenant_of.errors.add(:email, " value #{email} does not correspond to any user")
+      else
+        @tenant_of.user_id = user.id
+        @tenant_of.property_id = @property.id
+      end
+      
+      if @tenant_of.errors.count == 0 && @tenant_of.save
+        redirect_to property_path(@property), notice: 'Tenant of was successfully created.'
+      else
+        render action: 'new'
+      end 
+    end
+  end
+
+  # Only allows managers to delete their own tenants
   def destroy
-    @tenant_of.destroy
-    redirect_to :back, notice: 'Tenant was removed.'
+    if @tenant_of.property.user.id != @user.id
+      redirect_to properties_path, notice: "Not Authorized"
+    else
+      @tenant_of.destroy
+      redirect_to :back, notice: 'Tenant was removed.'  
+    end  
   end
 
   private
